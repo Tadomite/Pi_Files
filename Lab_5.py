5466ed17b0c9a6bea0ad13e9f19852dcb0a4aade
Tadomite <thaddster@gmail.com>
2025-10-06 14:49:03 -0400

    Added Script

--- Lab_5.py
+++ Lab_5.py
@@ -1,1 +1,9 @@
-
+import time
+import RPi.GPIO as GPIO
+import math
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.OUT,initial=1)
+pwm = GPIO.PWM(4,500)
+pwm.start(0.0)
+while(1):
+    pwm.ChangeDutyCycle(50+100*math.sin(2*3.14*.2*time.time()))
