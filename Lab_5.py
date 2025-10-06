ad07b737fd28c54f48f627b63476a76baa6af5af
Tadomite <thaddster@gmail.com>
2025-10-06 14:50:31 -0400

    push

--- Lab_5.py
+++ Lab_5.py
@@ -6,4 +6,5 @@ GPIO.setup(4,GPIO.OUT,initial=1)
 pwm = GPIO.PWM(4,500)
 pwm.start(0.0)
 while(1):
+    print(50+100*math.sin(2*3.14*.2*time.time()))
     pwm.ChangeDutyCycle(50+100*math.sin(2*3.14*.2*time.time()))
