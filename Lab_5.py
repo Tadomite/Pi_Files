05c2ad24b4de3c4a39fceaf1bb85bf0688f5abfe
Tadomite <thaddster@gmail.com>
2025-10-06 15:29:25 -0400

    fix

--- Lab_5.py
+++ Lab_5.py
@@ -1,14 +1,18 @@
 import time
 import RPi.GPIO as GPIO
 import math
-
+pins = [4,17,27,22,10,9,11,5,6,13]
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.OUT,initial=1)
-pwm = GPIO.PWM(4,500)
-pwm.start(0.0)
+pwm = []
+for i in pins:
+    GPIO.setup(i,GPIO.OUT,initial=1)
+    nPwm = GPIO.PWM(i,500)
+    nPwm.start(0.0)
+    pwm.append(nPwm)
 try:
  while(1):
-    pwm.ChangeDutyCycle(50+50*math.sin(2*3.14*.2*time.time()))
+    for i,j in enumerate(pwm):
+        j.ChangeDutyCycle(50+50*math.sin(2*3.14*.2*time.time()+i*3.14/11))
 except:
  GPIO.cleanup()
     
