748278e4eba7a56855d53318d54303e39fdd9120
Tadomite <thaddster@gmail.com>
2025-10-06 15:25:44 -0400

    multiple pins

--- Lab_5.py
+++ Lab_5.py
@@ -1,10 +1,14 @@
 import time
 import RPi.GPIO as GPIO
 import math
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.OUT,initial=1)
 pwm = GPIO.PWM(4,500)
 pwm.start(0.0)
-while(1):
-    print(50+50*math.sin(2*3.14*.2*time.time()))
+try:
+ while(1):
     pwm.ChangeDutyCycle(50+50*math.sin(2*3.14*.2*time.time()))
+except:
+ GPIO.cleanup()
+    
